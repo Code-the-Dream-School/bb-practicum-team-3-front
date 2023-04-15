45f89c15fe4a0b60fe59aca5ea10c71d3e72b0bb
Miguel <74852114+madebymiguel@users.noreply.github>
2023-04-15 12:49:03 -0700

    Change div to Box

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,15 @@
+import React from "react";
+import { Box } from "@mui/material";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import SearchForm from "../components/SearchForm";
 
 export default function HomePage() {
   return (
-    <div>
+    <Box>
       <Header />
       <SearchForm />
       <Footer />
-    </div>
+    </Box>
   );
 }
