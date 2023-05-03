c25c7d176b4efd10e462c60c1a71baf824969647
Miguel <74852114+madebymiguel@users.noreply.github>
2023-05-02 22:05:56 -0700

    Add error component.

---
+++ src/components/Error.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Warning from "../assets/warning.svg";
+
+export default function Error() {
+  return (
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      textAlign="center"
+      spacing={1}
+      sx={{ width: "100%", mt: "40px" }}
+    >
+      <img
+        src={Warning}
+        alt={"warning exclamation mark"}
+        loading="lazy"
+        style={{ width: "80px", height: "80px", marginBottom: "8px" }}
+      />
+      <Typography variant="h4" color="black">
+        {"Oh No!"}
+      </Typography>
+      <Typography variant="h5" color="error">
+        {"Something went wrong. Please try again later."}
+      </Typography>
+    </Stack>
+  );
+}
